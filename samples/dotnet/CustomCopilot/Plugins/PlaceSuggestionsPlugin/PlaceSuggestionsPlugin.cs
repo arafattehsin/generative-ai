2140920eacef8dd9e2f160ff134601e30c0f7781
Arafat Tehsin <arafattehsin@hotmail.com>
2024-03-11 16:56:44 +1100

    chore: add Custom Copilot sample and also fixed the folder structure

---
+++ samples/dotnet/CustomCopilot/Plugins/PlaceSuggestionsPlugin/PlaceSuggestionsPlugin.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Maps.Search.Models;
+using Azure.Maps.Search;
+using Azure;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CustomCopilot.Plugins.PlaceSuggestionsPlugin
+{
+    public class PlaceSuggestionsPlugin
+    {
+        MapsSearchClient client;
+        HttpClient httpClient = new HttpClient();
+        string APIKey;
+
+        public PlaceSuggestionsPlugin(string apiKey)
+        {
+            APIKey = apiKey;
+            AzureKeyCredential credential = new(apiKey);
+            client = new MapsSearchClient(credential);
+        }
+
+        [KernelFunction, Description("Gets the place suggestions for a given location")]
+        [return: Description("Place suggestions")]
+        public async Task<string> GetPlaceSuggestionsAsync(
+        [Description("type of the place")] string placeType,
+        [Description("name of the location")] string locationName)
+        {
+            var searchResult = await client.SearchAddressAsync(locationName);
+
+            if (searchResult?.Value?.Results.Count() == 0) { return null; }
+
+            SearchAddressResultItem locationDetails = searchResult!.Value.Results[0];
+
+            string url = @$"https://atlas.microsoft.com/search/fuzzy/json?api-version=1.0&query={placeType}
+                    &subscription-key={APIKey}
+                    &lat={locationDetails.Position.Latitude}
+                    &lon={locationDetails.Position.Longitude}
+                    &countrySet=AU
+                    &language=en-AU";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+    }
+}
---
+++ samples/dotnet/CustomCopilot/Plugins/WeatherPlugin/WeatherPlugin.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCopilot.Plugins.WeatherPlugin
+{
+    public class WeatherPlugin(string apiKey)
+    {
+        HttpClient client = new HttpClient();
+
+        [KernelFunction, Description("Gets the weather details of a given location")]
+        [return: Description("Weather details")]
+        public async Task<string> GetWeatherAsync(
+        [Description("name of the location")] string locationName)
+        {
+            string url = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={locationName}&aqi=no";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+    }
+}
