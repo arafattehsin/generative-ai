46092061dc10453209d8214f21d68909027d6dd5
Arafat Tehsin <arafattehsin@hotmail.com>
2024-09-09 21:39:14 +1000

    chore: add local rag sample

---
+++ samples/dotnet/local-rag-sk/Helpers/MemoryHelper.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.SemanticKernel.Memory;
+
+#pragma warning disable SKEXP0001
+
+namespace local_rag_sk.Helpers
+{
+
+    internal static class MemoryHelper
+    {
+        public static async void PopulateInterestingFacts(SemanticTextMemory memory, string collectionName)
+        {
+            var facts = OrgFact.GetFacts();
+            foreach (OrgFact fact in facts)
+            {
+                await memory.SaveInformationAsync(collection: collectionName, 
+                    id: fact.Id, 
+                    text: fact.Text);
+            }
+        }
+    }
+
+    public class OrgFact
+    {
+        public string Text { get; }
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string Description { get; }
+        public string AdditionalMetadata { get; }
+
+        public OrgFact(string text, string description, string additionalMetadata)
+        {
+            Text = text;
+            Description = description;
+            AdditionalMetadata = additionalMetadata;
+        }
+
+        public static IEnumerable<OrgFact> GetFacts()
+        {
+            var facts = new OrgFact[]
+                {
+                    new("Our headquarters is located in Sydney, Australia.", "Headquarters", "City: Sydney"),
+                    new("We have been in business for 25 years.", "Years in Operation", "Years: 25"),
+                    new("Our corporate sponsor is the Melbourne Football Club.", "Corporate Sponsorship", "Team: Melbourne Football Club"),
+                    new("We have 2 major departments.", "Departments", "Number: 2"),
+                    new("Our team includes developers among other professionals.", "Occupation", "Job Title: Developer"),
+                    new("Our team enjoys outdoor activities such as bushwalking.", "Team Activities", "Activity: Bushwalking"),
+                    new("We have a company pet policy that allows dogs.", "Company Pet Policy", "Type: Dog"),
+                    new("We prefer catering options featuring Australian cuisine.", "Catering Preferences", "Cuisine: Australian"),
+                    new("We have expanded our operations to 5 countries.", "International Presence", "Countries: 5"),
+                    new("Our staff includes graduates from the University of Sydney.", "Education", "University: Sydney"),
+                    new("Our team is multilingual, speaking 3 languages.", "Languages Spoken", "Number: 3"),
+                    new("We have a strict allergen policy, including precautions for peanuts.", "Allergen Policy", "Allergen: Peanuts"),
+                    new("We support athletic achievements, such as participating in marathons.", "Athletic Support", "Event: Marathon"),
+                    new("We have a company-wide collection of Australian art.", "Company Initiatives", "Item: Australian Art"),
+                    new("Our team enjoys the Australian spring season for company events.", "Seasonal Preferences", "Season: Spring"),
+                    new("Our corporate book club's favorite book is 'The Book Thief'.", "Corporate Book Club", "Book: The Book Thief"),
+                    new("We offer vegetarian, vegan, gluten free and halal options in our corporate diet policy.", "Dietary Policies", "Diet: Vegetarian"),
+                    new("We actively support volunteering in local community projects.", "Community Engagement", "Place: Local Community Projects"),
+                    new("We aim to expand our presence to every continent.", "Expansion Goals", "Goal: Every Continent"),
+                    new("Many of our staff members hold advanced degrees, including in Computer Science.", "Advanced Education", "Degree: Master's in Computer Science")
+                };
+            return facts;
+        }
+    }
+}
---
+++ samples/dotnet/local-rag-sk/Program.cs
@@ -0,0 +1,95 @@
+﻿#pragma warning disable SKEXP0070
+#pragma warning disable SKEXP0050
+#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0010
+#pragma warning disable SKEXP0020
+
+using local_rag_sk.Helpers;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Embeddings;
+using Microsoft.KernelMemory.SemanticKernel;
+using Microsoft.KernelMemory.AI.OpenAI;
+
+// Your PHI-3 model location 
+var phi3modelPath = @"D:\models\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
+var bgeModelPath = @"D:\models\bge-micro-v2\onnx\model.onnx";
+var vocabPath = @"D:\models\bge-micro-v2\vocab.txt";
+
+// Load the model and services
+var builder = Kernel.CreateBuilder();
+builder.AddOnnxRuntimeGenAIChatCompletion("phi-3", phi3modelPath);
+builder.AddBertOnnxTextEmbeddingGeneration(bgeModelPath, vocabPath);
+
+// Build Kernel
+var kernel = builder.Build();
+
+// Create services such as chatCompletionService and embeddingGeneration
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var embeddingGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
+
+var config = new SemanticKernelConfig(); ;
+var memory = new KernelMemoryBuilder()
+    .WithSemanticKernelTextGenerationService(new OnnxRuntimeGenAITextCompletionService("phi-3", phi3modelPath), config, new GPT4Tokenizer())
+    .WithSemanticKernelTextEmbeddingGenerationService(embeddingGenerator, config, new GPT4Tokenizer())
+    .WithSimpleVectorDb()
+    .Build<MemoryServerless>();
+
+var memoryPlugin = kernel.ImportPluginFromObject(new MemoryPlugin(memory, waitForIngestionToComplete: true), "memory");
+
+await memory.ImportWebPageAsync("https://raw.githubusercontent.com/arafattehsin/ParkingGPT/main/README.md", documentId: "doc001"); 
+await memory.ImportDocumentAsync($"Documents/HR Policy.docx");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("""
+ 
+  _                    _   ____      _    ____            _ _   _       __  __                           _           
+ | |    ___   ___ __ _| | |  _ \    / \  / ___| __      _(_) |_| |__   |  \/  | ___ _ __ ___   ___  _ __(_) ___  ___ 
+ | |   / _ \ / __/ _` | | | |_) |  / _ \| |  _  \ \ /\ / / | __| '_ \  | |\/| |/ _ \ '_ ` _ \ / _ \| '__| |/ _ \/ __|
+ | |__| (_) | (_| (_| | | |  _ <  / ___ \ |_| |  \ V  V /| | |_| | | | | |  | |  __/ | | | | | (_) | |  | |  __/\__ \
+ |_____\___/ \___\__,_|_| |_| \_\/_/   \_\____|   \_/\_/ |_|\__|_| |_| |_|  |_|\___|_| |_| |_|\___/|_|  |_|\___||___/
+                                                                                                    by Arafat Tehsin              
+""");
+
+
+// Start the conversation
+while (true)
+{
+    // Get user input
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("User > ");
+    var question = Console.ReadLine()!;
+
+    // Settings for the Phi-3 execution
+    OpenAIPromptExecutionSettings executionSettings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions,
+        MaxTokens = 100
+    };
+
+    // Invoke the kernel with the user input
+    var response = kernel.InvokePromptStreamingAsync(
+        promptTemplate: @"Question: {{$input}}
+        Answer the question using the memory content: {{memory.ask $input}}",
+        arguments: new KernelArguments(executionSettings)         
+        {
+            { "input", question }
+        }
+        );
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("\nAssistant > ");
+
+    //var result = response.Result.GetValue<string>();
+    string combinedResponse = string.Empty;
+    await foreach (var message in response)
+    {
+        //Write the response to the console
+        Console.Write(message);
+        combinedResponse += message;
+    }
+
+    Console.WriteLine();
+}
