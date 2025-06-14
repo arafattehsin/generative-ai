a46a89aaf9f77edb0de51c6db7d2f442e56d6061
Arafat Tehsin <arafattehsin@hotmail.com>
2025-06-14 11:04:03 +1000

    chore: update semantic kernel notebooks for dotnet with the latest code for azure ai foundry agents support

--- notebooks/semantic-kernel/dotnet/Helpers/GettingStarted.cs
+++ notebooks/semantic-kernel/dotnet/Helpers/GettingStarted.cs
@@ -1,42 +1,15 @@
-#pragma warning disable SKEXP0110, SKEXP0001
-using Microsoft.SemanticKernel.Agents;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Microsoft.SemanticKernel.Connectors.Google;
+#pragma warning disable SKEXP0110
+#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0002
+
 using Microsoft.SemanticKernel;
-using Kernel = Microsoft.SemanticKernel.Kernel;
-using System;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using ChatTokenUsage = OpenAI.Chat.ChatTokenUsage;
+using System.Text.Json;
 
 public class GettingStarted
 {
-    string azureOpenAIKey = Environment.GetEnvironmentVariable("AOI_KEY_SWDN");
-    string azureOpenAIEndpoint = Environment.GetEnvironmentVariable("AOI_ENDPOINT_SWDN");
-    string googleGeminiAPIKey = Environment.GetEnvironmentVariable("GOOGLE_GEMINI_KEY");
-    string ollamaEndpoint = "http://localhost:11434";
 
-    public static async Task InvokeAgentAsync(string input, ChatCompletionAgent agent, ChatHistory chat)
-    {
-        ChatMessageContent message = new(AuthorRole.User, input);
-        chat.Add(message);
-
-        await foreach (ChatMessageContent response in agent.InvokeAsync(chat))
-        {
-            chat.Add(response);
-            Console.WriteLine(response);
-        }
-    }
-
-    public Kernel CreateKernelWithChatCompletion()
-    {
-        var builder = Kernel.CreateBuilder();
-
-        builder.AddAzureOpenAIChatCompletion("gpt-4", azureOpenAIEndpoint, azureOpenAIKey, "gpt-4-service");
-
-#pragma warning disable SKEXP0070
-        builder.AddGoogleAIGeminiChatCompletion(modelId: "gemini-1.5-flash-latest", apiKey: googleGeminiAPIKey, apiVersion: GoogleAIVersion.V1_Beta, serviceId: "gemini-service");
-
-        builder.AddOllamaChatCompletion("llama3.2", new Uri(ollamaEndpoint), "ollama-service");
-
-        return builder.Build();
-    }
 }
